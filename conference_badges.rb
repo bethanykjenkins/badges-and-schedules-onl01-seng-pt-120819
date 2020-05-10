8dc5719a244c38c9e8ba55b3dfb217351ec2ac95
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2020-05-10 02:54:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,9 +5,5 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.map do |names|
-    greeting = "Hello, my name is #{name}."
-    messages << greeting
-  end
-  list
+  array.map {|names| "Hello, my name is #{name}."}
 end
