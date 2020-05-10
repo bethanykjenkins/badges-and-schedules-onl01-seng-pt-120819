06f92eb49207e8c0158ad8e86fcefa40b35829a1
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2020-05-10 02:59:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(speakers)
-  speakers.eachwithindex do |name|
+  speakers.each_with_index.map do |name, index|
     puts "Hello, #{name}! You'll be assigned to room #{index + 1}."
   end
 end
